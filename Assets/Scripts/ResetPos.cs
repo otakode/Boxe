dba2e93fc62095133b2ca9a098421ca218506f62
Otakode <marchal.cl@gmail.com>
2014-01-21 10:03:30 +0100

    initial commit
    Signed-off-by: Otakode <marchal.cl@gmail.com>

---
+++ Assets/Scripts/ResetPos.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetPos : MonoBehaviour
+{
+	void Start ()
+	{
+		this.transform.localPosition = Vector3.zero;
+		this.transform.localRotation = Quaternion.identity;
+		this.transform.localScale = Vector3.one * 0.2f;
+	}
+}
