7c7d4be658d073503af59de19f52b58771d057b6
Otakode <marchal.cl@gmail.com>
2014-01-22 11:33:59 +0100

    new animations
    Signed-off-by: Otakode <marchal.cl@gmail.com>

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour
+{
+	private PXCUPipeline.Mode	ppMode = PXCUPipeline.Mode.FACE_LOCATION |
+										 PXCUPipeline.Mode.GESTURE;
+	private PXCUPipeline		pp = null;
+
+	public GameObject	Left;
+	public GameObject	Right;
+	public GameObject	Head;
+
+	void Start()
+	{
+		this.pp = new PXCUPipeline();
+		if (this.pp == null || !this.pp.Init(this.ppMode))
+		{
+			Debug.Log("PXCUPipeline Initialization ERROR");
+			return;
+		}
+	}
+
+	void OnDestroy()
+	{
+		this.pp.Close();
+	}
+
+	void Update()
+	{
+		if (!this.pp.AcquireFrame(false))
+		{
+		//	Debug.Log("PXCUPipeline AcquireFrame ERROR");
+			return;
+		}
+		this.UpdatePlayer();
+		this.pp.ReleaseFrame();
+	}
+
+	void UpdatePlayer()
+	{
+		this.UpdateFists();
+		this.UpdateHead();
+	}
+
+	void UpdateFists()
+	{
+		PXCMGesture.GeoNode leftHand;
+		PXCMGesture.GeoNode rightHand;
+		this.pp.QueryGeoNode(PXCMGesture.GeoNode.Label.LABEL_BODY_HAND_LEFT, out leftHand);
+		this.pp.QueryGeoNode(PXCMGesture.GeoNode.Label.LABEL_BODY_HAND_RIGHT, out rightHand);
+		if (leftHand.positionWorld.x < rightHand.positionWorld.x)
+		{
+			PXCMGesture.GeoNode temp = leftHand;
+			leftHand = rightHand;
+			rightHand = temp;
+		}
+		this.UpdateFist(leftHand, this.Left);
+		this.UpdateFist(rightHand, this.Right);
+	}
+
+	void UpdateFist(PXCMGesture.GeoNode fist, GameObject glove)
+	{
+		glove.transform.position = new Vector3(-fist.positionWorld.x * 2, fist.positionWorld.z * 2, -fist.positionWorld.y * 2);
+	}
+
+	void UpdateHead()
+	{
+		int faceID = 0;
+		ulong timestamp;
+		PXCMFaceAnalysis.Detection.Data face;
+		if (this.pp.QueryFaceID(0, out faceID, out timestamp) &&
+		    this.pp.QueryFaceLocationData(faceID, out face))
+		{
+			this.Head.transform.position = new Vector3(1.3f - (face.rectangle.x) / 200f, 1f - (face.rectangle.y) / 200f, this.Head.transform.position.z);
+		}
+		else
+		{
+			// place head between and behind the fists
+		}
+	}
+}
